7d5cea27aa8471cf86cff5ef81f21fc55081ed0a
david-habgood <david.habgood@surroundaustralia.com>
2021-03-11 16:43:27 +1000

    Added basic content negotiation Added bitbucket-pipelines.yml

---
+++ tests/test_placeholder.py
@@ -0,0 +1,18 @@
+"""
+REWRITE when code converted to fastAPI
+"""
+
+"""
+This Python 3.8 code tests the ``conversion_utils`` module.
+Beware, these tests cover only some functions and only some scenarios.
+Keep adding tests!
+CHANGELOG:
+- 2021-03-09:   David Habgood (DH): Initial version
+"""
+import unittest
+
+
+class ConversionTestCase(unittest.TestCase):
+
+    def test(self):
+        assert 1+1 == 2
--- views/home.py
+++ views/home.py
@@ -1,11 +1,14 @@
-import fastapi
-from starlette.requests import Request
 from starlette.templating import Jinja2Templates
-from fastapi import APIRouter
+from fastapi import APIRouter, Header
+from app.renderers import render, JSONRenderer, PlainTextRenderer, HTMLRenderer
+from typing import Optional
 
 templates = Jinja2Templates('templates')
 router = APIRouter()
 
-@router.get('/')
-async def index(request: Request):
-    return templates.TemplateResponse('api.html', {'request': request})
+@router.get("/")
+async def index(accept: Optional[str] = Header(default='text/plain')):
+    data = 'test'
+    return render(
+        data, accept, status_code=200,
+        renderers=[JSONRenderer, PlainTextRenderer, HTMLRenderer])
